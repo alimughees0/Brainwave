838f06ad02e8da4fd6b3a6c494de8d9e24c46700
AliMughees <58826889+syedalimughees@users.noreply.github.com>
2024-03-27 12:56:16 +0500

    First commit

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Benefits() {
+  return (
+    <View>
+      <Text>Benefits</Text>
+    </View>
+  );
+}
